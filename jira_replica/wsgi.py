36c76c0d1ce840760c69e033c161f287ae904d69
Gokulakrishnan1998 <krishnangokul1998ks@gmail.com>
2024-11-27 17:37:59 +0530

    Added requirements

--- jira_replica/wsgi.py
+++ jira_replica/wsgi.py
@@ -6,9 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
+import sys
 import os
 
+project_path = '/home/Gokulakrishnan/replica'
+if project_path not in sys.path:
+    sys.path.append(project_path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jira_replica.settings')
